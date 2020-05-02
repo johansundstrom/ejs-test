416d3b0a318591fea983bd219324b6d9ce791ae4
Johan sundström <johan.sundstrom@mdh.se>
2020-05-02 14:36:22 +0200

    fixed css

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app = express()
 
 const apiKey = '9955c7404e74d62e8783615208bc0c59';
 
-app.use(express.static('public'));
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
